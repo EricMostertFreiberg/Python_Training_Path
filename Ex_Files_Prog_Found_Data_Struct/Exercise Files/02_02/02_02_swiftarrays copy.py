8f170b240e8d5066eaf5c037f8c205cb0a89308e
Eric Mostert-Freiberg <Eric.MFreiberg@gmail.com>
2022-04-11 14:03:49 -0700

    New file

---
+++ Ex_Files_Prog_Found_Data_Struct/Exercise Files/02_02/02_02_swiftarrays copy.py
@@ -0,0 +1,21 @@
+perStudentPetCount = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
+
+numOfStudents = perStudentPetCount.count
+
+# sumOfItems / numOfStudents
+
+print(perStudentPetCount[3])
+# print(perStudentPetCount[100])
+print(numOfStudents)
+
+sum = 0
+
+for individualPetCount in perStudentPetCount {
+    sum = sum + individualPetCount
+}
+
+print(sum)
+
+average = sum / numOfStudents
+
+print(average)
